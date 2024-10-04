c80cde49b93ccd5449e899ccd9ef67152e6e4ec8
Nicholas Chen <nichwch@gmail.com>
2024-10-03 21:48:09 -0400

    style summarizer page

--- visualizer/app/page.tsx
+++ visualizer/app/page.tsx
@@ -19,25 +19,12 @@ interface WeeklySummary {
 
 export default function Home() {
   const [summaries, setSummaries] = useState<WeeklySummary[]>([]);
-  const [expandedWeeks, setExpandedWeeks] = useState<Set<string>>(new Set());
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/weekly_summaries")
       .then((response) => response.json())
       .then((data) => setSummaries(data));
   }, []);
-
-  const toggleWeek = (week: string) => {
-    setExpandedWeeks((prev) => {
-      const newSet = new Set(prev);
-      if (newSet.has(week)) {
-        newSet.delete(week);
-      } else {
-        newSet.add(week);
-      }
-      return newSet;
-    });
-  };
 
   const formatWeekDate = (timestamp: number) => {
     const date = new Date(timestamp * 1000);
@@ -46,34 +33,51 @@ export default function Home() {
       day: "numeric",
       year: "numeric",
     };
-    return `Week of ${date.toLocaleDateString("en-US", options)}`;
+    return date.toLocaleDateString("en-US", options);
   };
 
   return (
-    <div className="min-h-screen p-8">
-      {summaries
-        .slice()
-        .reverse()
-        .map((summary) => (
-          <div key={summary.week} className="mb-6 p-4">
-            <h2
-              className="
-              s mb-2 cursor-pointer"
-              onClick={() => toggleWeek(summary.week)}
+    <>
+      <div className="min-h-screen mx-auto w-full lg:w-[900px]">
+        <div className="fixed z-[99999] top-0 h-[30px] bg-orange-200 border border-black px-2">
+          summarizer
+        </div>
+        {summaries
+          .slice()
+          .reverse()
+          .map((summary) => (
+            <div
+              key={summary.week}
+              className="mb-6 mt-10 bg-orange-200 relative shadow-md"
             >
-              {formatWeekDate(summary.timestamp)}
-              {expandedWeeks.has(summary.week) ? " v" : " ^"}
-            </h2>
-            {expandedWeeks.has(summary.week) && (
-              <div className="whitespace-pre-wrap flex gap-4">
+              <h2
+                className={`sticky z-50 top-[30px] px-2 border border-black bg-orange-200 shadow-md`}
+              >
+                Week of{" "}
+                <span className="underline">
+                  {formatWeekDate(summary.timestamp)}
+                </span>
+              </h2>
+              <div className="whitespace-pre-wrap flex border-x border-x-black border-b border-b-black">
                 {summary.summary.overall_summary && (
-                  <div className="basis-1/3 shrink-0 border border-black p-2">
-                    <h1 className="text-lg mb-2">overall</h1>
+                  <div className="basis-1/3 shrink-0 border-r border-r-black p-2">
                     <div>{summary.summary.overall_summary}</div>
                   </div>
                 )}
-                <div className="flex flex-wrap gap-4">
+                <div className="flex flex-wrap gap-4 p-3 bg-orange-400/30 shadow-inner grow">
                   {Object.entries(summary.summary).map(([key, value]) => {
+                    const bgColor =
+                      key === "dreams"
+                        ? "bg-purple-100"
+                        : key === "life"
+                        ? "bg-green-100"
+                        : key === "ideas"
+                        ? "bg-blue-100"
+                        : key === "gratitude"
+                        ? "bg-pink-200"
+                        : key === "complaints"
+                        ? "bg-red-300"
+                        : null;
                     if (
                       key !== "overall_summary" &&
                       key !== "main_themes" &&
@@ -83,10 +87,22 @@ export default function Home() {
                       return (
                         <div
                           key={key}
-                          className="w-[250px] h-[250px] border overflow-y-auto border-black p-2"
+                          className={`w-[250px] h-[250px] border 
+                          overflow-y-auto border-black 
+                          shadow-md
+                          relative 
+                          ${bgColor}`}
                         >
-                          <h1 className="text-lg mb-2">{key}</h1>
-                          <div>{value}</div>
+                          <h1
+                            className={`text-lg mb-2 border-b
+                             border-b-black sticky top-0
+                             px-2
+                         ${bgColor} 
+                          `}
+                          >
+                            {key}
+                          </h1>
+                          <div className="px-2">{value}</div>
                         </div>
                       );
                     }
@@ -94,9 +110,9 @@ export default function Home() {
                   })}
                 </div>
               </div>
-            )}
-          </div>
-        ))}
-    </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
